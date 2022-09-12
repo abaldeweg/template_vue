1d935b341df18361e8c4b08f4f48ec990b90c924
André Baldeweg <a@baldeweg.org>
2022-09-12 15:09:37 +0000

    init

---
+++ src/views/IndexView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useTitle } from '@baldeweg/ui'
+import { useI18n } from 'vue-i18n'
+
+defineProps({
+  auth: Object,
+})
+
+const { t } = useI18n()
+
+useTitle({ title: t('welcome') })
+</script>
+
+<template>
+  <BContainer size="m">
+    <h1>{{ $t('welcome') }}</h1>
+  </BContainer>
+</template>
